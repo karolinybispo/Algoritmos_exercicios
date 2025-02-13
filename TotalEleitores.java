f0f66cad2a26aae29df6a47ab78d32f6a733dc8b
karoliny bispo <karol-bobio@hotmail.com>
2025-02-13 20:18:44 -0300

    sem o calculo

---
+++ TotalEleitores.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TotalEleitores {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("digite o numero total de eleitores: ");
+        int eleitores = scanner.nextInt();
+
+        System.out.print("numero de votos brancos");
+        int votoBranco = scanner.nextInt();
+
+        System.out.print("numero de votos nulos");
+        int votoNulo = scanner.nextInt();
+
+        System.out.print("numero de votos validos");
+        int votoValido = scanner.nextInt();
+
+        //voce nao finalizou, sua logica esta no papel solto dentro do caderno da facul
+    }
+}
